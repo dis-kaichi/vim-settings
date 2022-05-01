0f41f2eeebf2034a298a979a6f01b962b7d6384d
dis-kaichi <dis-kaichi@sample.com>
2022-05-01 17:16:45 +0900

    新コマンド色々追加

---
+++ bin/initialize-dart-syntax.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p ~/.vim/syntax
+ln -s ~/my/modules/vim/dart-vim-plugin/syntax/dart.vim ~/.vim/syntax/
+
---
+++ bin/startup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+MYBASE=/home/haoushou/my
+
+# Keyboard
+. $MYBASE/bin/change-control.sh
+# Guake
+. $MYBASE/bin/drive-guake.sh
+
